ac67edb023abe563233a009024dc9d4ba6208041
Anup Deshmukh <anupdeshmukh@Anups-MacBook-Air.local>
2025-11-09 13:57:51 +0530

    updated deps and test file

--- test/cdk-workshop.test.ts
+++ test/cdk-workshop.test.ts
@@ -1,17 +1,44 @@
 import * as cdk from 'aws-cdk-lib';
 import { Template, Match } from 'aws-cdk-lib/assertions';
-import * as CdkWorkshop from '../lib/cdk-workshop-stack';
+import { CdkWorkshopStack } from '../lib/cdk-workshop-stack';
 
-test('SQS Queue and SNS Topic Created', () => {
+test('Stack wires API Gateway through hit counter and downstream Lambda', () => {
   const app = new cdk.App();
-  // WHEN
-  const stack = new CdkWorkshop.CdkWorkshopStack(app, 'MyTestStack');
-  // THEN
-
+  const stack = new CdkWorkshopStack(app, 'MyTestStack');
   const template = Template.fromStack(stack);
 
-  template.hasResourceProperties('AWS::SQS::Queue', {
-    VisibilityTimeout: 300
+  template.resourceCountIs('AWS::ApiGateway::RestApi', 1);
+
+  template.resourceCountIs('AWS::DynamoDB::Table', 1);
+  template.hasResourceProperties('AWS::DynamoDB::Table', {
+    KeySchema: [
+      {
+        AttributeName: 'path',
+        KeyType: 'HASH'
+      }
+    ],
+    AttributeDefinitions: [
+      {
+        AttributeName: 'path',
+        AttributeType: 'S'
+      }
+    ]
   });
-  template.resourceCountIs('AWS::SNS::Topic', 1);
+
+  template.hasResourceProperties(
+    'AWS::Lambda::Function',
+    Match.objectLike({
+      Handler: 'hitcounter.handler',
+      Environment: {
+        Variables: {
+          DOWNSTREAM_FUNCTION_NAME: Match.anyValue(),
+          HITS_TABLE_NAME: Match.anyValue()
+        }
+      }
+    })
+  );
+
+  template.hasResourceProperties('AWS::Lambda::Function', {
+    Handler: 'hello.handler'
+  });
 });
